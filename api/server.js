5f657ab56a5dd082d1452044c4be7334dbe0fb02
bevanger <baileyevanger@gmail.com>
2021-08-30 16:49:28 -0600

    created put endpoint

--- api/server.js
+++ api/server.js
@@ -65,11 +65,24 @@ server.delete('/api/users/:id', (req, res) => {
         })
 });
 //[PUT] updates user
-server.put('/api/users/:id', (req, res) => {
-        
-});
+server.put('/api/users/:id', async (req, res) => { 
+    try {
+        const oldUser = await User.findById(req.params.id)
+        if(!oldUser) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({ message: "Please provide name and bio for the user" })
+            } else {
+                const updatedUser = await User.update(req.params.id, req.body)
+                    res.status(200).json(updatedUser)
+            }
+        } 
 
-
+        } catch (err) {
+            res.status(500).json({ message: "The user information could not be modified" })
+        }
+    });
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
