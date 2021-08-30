b2c00b18192a6af58fa5252f33a20de9d7a61f04
bevanger <baileyevanger@gmail.com>
2021-08-30 13:52:05 -0600

    started to create the endpoints and made sure they were connecting properly

--- api/server.js
+++ api/server.js
@@ -1,3 +1,33 @@
 // BUILD YOUR SERVER HERE
+//Imports
+const express = require('express');
+//instance of express app
+const server = express();
+//global middleware
+server.use(express.json());
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+//[POST] creates new user
+server.post('/api/users', (req, res) => {
+    res.json('creates new user')
+});
+//[GET] returns array of users
+server.get('/api/users', (req, res) => {
+    res.json('returns array of users')
+});
+//[GET] returns user object with id 
+server.get('/api/users/:id', (req, res) => {
+    res.json('returns user object with id')
+});
+//[DELETE] removes user with id
+server.delete('/api/users/:id', (req, res) => {
+    res.json('deletes user')
+});
+//[PUT] updates user
+server.put('/api/users/:id', (req, res) => {
+    res.json('updates user')
+});
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
